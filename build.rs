34b75f25c4d2ff2657441bab818c6007af0406fa
Nixon <nixon.emoony@gmail.com>
2019-04-11 10:18:05 +0100

    Nicen up

--- build.rs
+++ build.rs
@@ -3,36 +3,44 @@ extern crate bindgen;
 use std::env;
 use std::path::PathBuf;
 
-use std::io::prelude::*;
-use walkdir::{DirEntry, WalkDir};
+//use std::io::prelude::*;
 use std::process::Command;
 
+const H_FILE_NAME: &str = "/libpros.h";
+const CC: &str = "gcc";
+
 fn main() {
-    
+    // We only want to rerun the build process if the include dir has changes
+    println!("cargo:rerun-if-changed=include");
+
+    println!("Generating PROS bindings");
+
     let out_dir =env::var("OUT_DIR").unwrap();
-    let out_h_loc = out_dir + "/libpros.h";
+    let out_h_loc = out_dir + H_FILE_NAME;
+
     // Preprocess the c code
-    Command::new("gcc")
+    Command::new(CC)
+        // Set the include path
         .arg("-I").arg("./include/")
+        // Input rust.h
         .arg("-E").arg("include/rust.h")
+        // Output to somewhere in target
         .arg("-o").arg(out_h_loc.clone())
         .output()
         .expect("failed to preprocess code");
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
-    let bindings = bindgen::Builder::default()
 
+    let bindings = bindgen::Builder::default()
         .header(out_h_loc.clone())
-        // Finish the builder and generate the bindings.
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+
+    println!("Generated PROS bindings")
 }
