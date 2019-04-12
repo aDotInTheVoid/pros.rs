8cb090fc14e1875c83a713de21c8970a695b0439
Nixon <nixon.emoony@gmail.com>
2019-04-12 19:12:47 +0100

    compile a staticlib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
 #![no_std]
+#![crate_type = "staticlib"]
 
 mod pros;
+
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
